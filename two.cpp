7faed5ecb78e28c1e1b69c94182a280b6696b612
Tushar Saxena <stushar345@gmail.com>
2021-03-17 23:44:45 +0530

    Update two.cpp

--- two.cpp
+++ two.cpp
@@ -1,34 +1,15 @@
-#include<iostream>
-using namespace std;
-int main()
+ vector<int> spirallyTraverse(vector<vector<int> > mat, int m, int n) 
  {
-     int p;
-     cin>>p;
-while(p--)
-{
-    int m,n;
-    cin>>m>>n;
-    long long int mat[m][n];
-    for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            cin>>mat[i][j];
-        }
-    }
-
 int top=0;
 int down=m-1,left=0,right=n-1,dir=0;
-int arr[m*n]={0};
-int k=0;
+vector<int> arr;
 while(top<=down && left<=right)
 {
     if(dir==0)
     {
         for(int i=left;i<=right;i++)
         {
-            arr[k]=mat[top][i];
-            k++;
+            arr.push_back(mat[top][i]);
         }
         top=top+1;
     }
@@ -37,8 +18,7 @@ while(top<=down && left<=right)
     {
         for(int i=top;i<=down;i++)
         {
-            arr[k]=mat[i][right];
-            k++;
+            arr.push_back(mat[i][right]);
         }
         
         right=right-1;
@@ -48,8 +28,7 @@ while(top<=down && left<=right)
     {
         for(int i=right;i>=left;i--) 
         {
-            arr[k]=mat[down][i];
-            k++;
+            arr.push_back(mat[down][i]);
         }
         down=down-1;
         
@@ -59,8 +38,7 @@ while(top<=down && left<=right)
     {
        for(int i=down;i>=top;i--)
        {
-            arr[k]=mat[i][left];
-            k++;
+            arr.push_back(mat[i][left]);
        }
        left=left+1;
     }
@@ -68,10 +46,5 @@ while(top<=down && left<=right)
     dir=(dir+1)%4;
 
 }
-for(int i=0;i<k;i++)
-{
-    cout<<arr[i]<<" ";
-}
-cout<<endl;
-}
-}
+return arr;
+ }
